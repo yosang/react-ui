1a042bc532fe7bed8065f50d2ae22f1ab04d8286
Yosmel Chiang <yosmelchiang@gmail.com>
2026-03-30 01:48:47 +0200

    refactor: remove auth component

--- src/login.stories.jsx
+++ src/login.stories.jsx
@@ -1,5 +1,15 @@
-import { Auth } from "./layout-components/Auth";
+import { Button } from "./button/Button";
+import { Input } from "./Forms/Input";
+import { LoginContainer } from "./layout-components/LoginContainer";
 
-export function AuthForm() {
-    return <Auth authType="register" centered={true}/>
+export function AuthExample() {
+    return (
+    <LoginContainer centered={true}>
+        <p>Username</p>
+        <Input style={{ maxWidth: "200px"}}/>
+        <p>Password</p>
+        <Input style={{ maxWidth: "200px"}}/>
+        <Button>Login</Button>
+    </LoginContainer>
+    )
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // Components
 export * from "./button/Button.jsx";
 export * from "./Forms/Input.jsx";
+export * from "./layout-components/LoginContainer.jsx";
 
 // Icons
 export * from "./icons";
