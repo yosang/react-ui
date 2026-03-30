7f0efc6c2353db41b7f4b9242fa850ac338056af
Yosmel Chiang <yosmelchiang@gmail.com>
2026-03-30 16:17:20 +0200

    update navbar

--- src/layout-components/Navbar/NavLink.jsx
+++ src/layout-components/Navbar/NavLink.jsx
@@ -1,13 +1,10 @@
 import './NavLink.css'
 import '../../main.css'
-import { Ripple } from 'm3-ripple'
-import 'm3-ripple/ripple.css'
 
 export function NavLink({ children, href = "#", ...props }) {
     return (
         <li>
             <a className='ui-navlink' {...props} href={href} style={{ position: 'relative' }} >
-                <Ripple />
                 {children}
             </a>
         </li>
