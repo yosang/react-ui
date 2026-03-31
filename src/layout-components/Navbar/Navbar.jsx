579e420e13dbb69ab51b68153c87f06883ecf653
Yosmel Chiang <yosmelchiang@gmail.com>
2026-03-31 11:06:45 +0200

    refactor: simplify navbar component

--- src/layout-components/Navbar/Navbar.jsx
+++ src/layout-components/Navbar/Navbar.jsx
@@ -1,26 +1,30 @@
 import { Container } from '../Container/Container';
-import { NavItems } from './NavItems'
-import { NavLink } from './NavLink';
-import { Divider } from '../Divider/Divider';
-
+import './Navbar.css'
 import '../../main.css'
 
 
-export function Navbar({ children, sticky = true, ...props }) {
+export function Navbar({ children, sticky = false, ...props }) {
     return (
         <Container direction="row" style={sticky ? { position: "sticky", top: 0, zIndex: 1000 } : {}} {...props}>
-            <div>
-                Logo
-            </div>
-            <NavItems>
-                <NavLink>Home</NavLink>
-                <NavLink>About</NavLink>
-                <NavLink>Contact</NavLink>
-                {children && (
-                    <Divider direction='vertical' />
-                )}
-                {children}
-            </NavItems>
+            {children}
         </Container>
+    )
+}
+
+export function NavItems({ children, direction = "", ...props }) {
+    return (
+        <ul className={`ui-navitems ${direction == "vertical" ? "vertical" : ""}`} {...props} >
+            {children}
+        </ul>
+    )
+}
+
+export function NavLink({ children, href = "#", ...props }) {
+    return (
+        <li>
+            <a className='ui-navlink' {...props} href={href} >
+                {children}
+            </a>
+        </li>
     )
 }
--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ export * from "./layout-components/Container/Container.jsx";
 export * from "./layout-components/Divider/Divider.jsx";
 export * from "./layout-components/Modal/Modal.jsx";
 export * from "./layout-components/Navbar/Navbar.jsx";
-export * from "./layout-components/Navbar/NavItems.jsx";
-export * from "./layout-components/Navbar/NavLink.jsx";
 export * from "./layout-components/Spinner/Spinner.jsx";
 export * from "./layout-components/Table/Table.jsx";
 
