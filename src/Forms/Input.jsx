6271821d6839f0a2b86590eb1b0e725ab339b142
Yosmel Chiang <yosmelchiang@gmail.com>
2026-04-08 10:18:10 +0200

    feat: add label and error feedback to input

--- src/Forms/Input.jsx
+++ src/Forms/Input.jsx
@@ -1,12 +1,56 @@
 import './Input.css'
 import '../main.css'
+import {InfoIcon} from '../icons/forms/InfoIcon'
 
-export function Input({ variant = "primary", type ="text", placeholder, ...props}) {
+import { forwardRef, memo, useState } from 'react'
+
+const InputComponent = forwardRef(({ 
+    id,
+    variant = "primary", 
+    type ="text", 
+    placeholder, 
+    labelText,
+    labelPosition = "top",
+    error,
+
+    ...props}
+
+    , ref) => {
+    
+    const [randomId, setRandomId] = useState(crypto.randomUUID().toString())
+    
+    const inputId = id || randomId
+
+    const isRequired = props.required
+    const isDisabled = props.disabled
+
     return (
-        <input className={`ui-input ${variant}`} placeholder={placeholder} type={type } {...props} />
-    )
-}
+            <div className={`ui-input-wrapper ${
+                labelPosition === "left" ? "label-left":
+                labelPosition === "right" ? "label-right":""
+                 }
+            `}>
 
-// Todos: 
-// Add disabled functionality
-// Add icon inside the input functionality
+            {labelText && <label htmlFor={inputId}> {labelText} {error && <span title={error} className='error-message' ><InfoIcon /></span>} </label>}
+            
+            <input 
+            id={inputId}
+            ref={ref}
+            className={`ui-input ${variant} ${error ? "input-error":""}`} 
+            placeholder={placeholder} 
+            title={error}
+            type={type } 
+
+            // Accessibility
+            aria-required={isRequired}
+            aria-disabled={isDisabled}
+            aria-invalid={error}
+
+            {...props} 
+            />
+
+            </div>
+        )
+})
+
+export const Input = memo(InputComponent)
--- src/stories/input.stories.jsx
+++ src/stories/input.stories.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
 import { Input } from "../Forms/Input.jsx";
 
 export function TextInput() {
-    return <Input type="text" />
+    return <Input />
+}
+
+export function TextInputWithLabel() {
+    return <Input labelText="Username"/>
+}
+export function TextInputWithLabelLeft() {
+    return (
+        <>
+        <Input error="Bad shit" labelText="Username" labelPosition="left"/>
+        <Input labelText="Password" labelPosition="left"/>
+        <Input labelText="Email" labelPosition="left"/>
+        <Input labelText="A looong as text" labelPosition="left"/>
+        </>
+    )
+}
+export function TextInputWithLabelRight() {
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+        setTimeout(() => setError("bad shit"), 500)
+    }, [])
+
+    return (
+        <>
+        <Input error={error} labelText="Username" labelPosition="right"/>
+        <Input labelText="Password" labelPosition="right"/>
+        <Input labelText="Email" labelPosition="right"/>
+        <Input labelText="A looong as text" labelPosition="right"/>
+        </>
+    )
+}
+
+export function DisabledInput() {
+    return <Input disabled={true} labelText="Username"/>
+}
+export function InputWithAnError() {
+    return <Input error="Bad shit" labelText="Username"/>
 }
